f4974a12ab15fa5b02e01853ec481f1998638d80
Mykola Mamaiev <mamaievm@ae.com>
2024-01-24 13:10:51 +0200

    Project init.

---
+++ src/main/java/ua/mamaiev/springgurupetclinicfromscratch/SpringGuruPetClinicFromScratchApplication.java
@@ -0,0 +1,13 @@
+package ua.mamaiev.springgurupetclinicfromscratch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGuruPetClinicFromScratchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGuruPetClinicFromScratchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/mamaiev/springgurupetclinicfromscratch/SpringGuruPetClinicFromScratchApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.mamaiev.springgurupetclinicfromscratch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGuruPetClinicFromScratchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
